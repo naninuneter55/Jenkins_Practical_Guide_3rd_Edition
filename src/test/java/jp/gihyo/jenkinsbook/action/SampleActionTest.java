02319f966199d42fe46f92951cd639321588ea1b
naninuneter55 <naninuneter55@gmail.com>
2018-08-09 19:11:51 +0900

    Update SampleActionTest.java

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -59,7 +59,7 @@ public class SampleActionTest {
 		boolean result = action.checkParameter(request);
 		verify(request);
 
-		assertEquals(true, result);
+		assertEquals(false, result);
 	}
 	
 	@Test
