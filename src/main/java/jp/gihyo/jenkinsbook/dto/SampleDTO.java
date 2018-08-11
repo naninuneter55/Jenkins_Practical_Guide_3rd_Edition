2ba7b74705f35e112bfea124d9bb5d489eaaeb16
naninuneter55 <naninuneter55@gmail.com>
2018-08-11 15:55:07 +0900

    Update SampleDTO.java

--- src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
+++ src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * DTO class for SampleServlet.
  */
-public SampleDTO implements Serializable {
+public class SampleDTO implements Serializable {
 
     /**
      * First name of the user.
